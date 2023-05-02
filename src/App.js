00c228826b605138708334fc70e186dfa316b140
Shamsida A <87810860+Shamsida@users.noreply.github.com>
2023-05-02 20:21:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/login">Login</Link></li>
+          <li><Link to="/registration">Registration</Link></li>
         </ul>
       </nav>
       <Routes>
